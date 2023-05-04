100715c47af09dfd23bd9b999db0a543b4f79a93
sudani05 <132605679+sudani05@users.noreply.github.com>
2023-05-04 23:21:05 +0530

    Add files via upload

---
+++ b2.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int A, B, sum = 0;
+
+	
+	printf("Enter tha numbers A and B  \n");
+
+	scanf("%d%d", &A, &B);
+	sum = A + B;
+	printf("Sum of A and B  %d", sum);
+
+	return 0;
+}
---
+++ b3.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+	float A, B, sum =0;
+
+	printf("Enter two  number A and B \n");
+
+	scanf("%f%f", &A, &B);
+    sum = A * B;
+
+	printf("sum of A and B is: %f", sum);
+
+	return 0;
+}
